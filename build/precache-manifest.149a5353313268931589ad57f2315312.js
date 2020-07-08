14cda56ad9d3b332455da9586a4d02ed3ae817b1
unknown <amankmchoudhary@gmail.com>
2020-07-08 16:12:29 +0530

    alll done

---
+++ build/precache-manifest.149a5353313268931589ad57f2315312.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b484adecbcc3687e6c5b07576e3d697",
+    "url": "/funwithrobofriends/index.html"
+  },
+  {
+    "revision": "de2597fdb05c2feda2bb",
+    "url": "/funwithrobofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "456071d0609212d2e6da",
+    "url": "/funwithrobofriends/static/css/main.f31e89e8.chunk.css"
+  },
+  {
+    "revision": "de2597fdb05c2feda2bb",
+    "url": "/funwithrobofriends/static/js/2.da2c29ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funwithrobofriends/static/js/2.da2c29ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456071d0609212d2e6da",
+    "url": "/funwithrobofriends/static/js/main.a65b88be.chunk.js"
+  },
+  {
+    "revision": "68b9188d1992ebdd53a3",
+    "url": "/funwithrobofriends/static/js/runtime-main.f4fb4ead.js"
+  },
+  {
+    "revision": "6d80caebe163ffed4b7e4c058b9eb75f",
+    "url": "/funwithrobofriends/static/media/Folkner Stamp.6d80caeb.woff"
+  }
+]);
